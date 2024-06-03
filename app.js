58e6bbb4b702659719be0e5f858b23b7432785cb
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-06-03 12:07:09 -0700

    wrote websocket server and added prettier

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res) {
-res.write("On the way to being a full snack engineer!");
-res.end();	
-
-
-}
-).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("On the way to being a full snack engineer!");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("Server started on port http://localhost:3000/");
